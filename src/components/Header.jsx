23f564ad510759980a85fa1b7f2c3ce32b78f4a6
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-16 22:13:22 -0700

    header styling adjustments + animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.css'
-
+import macaron from './../assets/macaron.png'
 function Header() {
     return (
         <header className='site-header'>
-            <span>Baking Macarons</span>
+            <div className="logo-container">
+                <img className="macaron" src={macaron} alt="macaron" />
+                <span>Macarons</span>
+            </div>
+            
 
             <ul className='nav-links'>
                 <li>Recipes</li>
